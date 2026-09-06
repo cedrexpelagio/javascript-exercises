a4b638084825878bf2ab9c0929acfb70224ffc55
cedrexpelagio <cedrexpelagio@gmail.com>
2026-09-06 14:13:13 +0800

    Solved

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if (start < 0 && end < 0)
+        return "ERROR";
 
+    let total = 0;
+
+    for (let i = start; i <= end; i++){
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line

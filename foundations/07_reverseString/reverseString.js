361bcbc23c9b2e4fbf570053724c6384e994fd2b
cedrexpelagio <cedrexpelagio@gmail.com>
2026-09-06 13:45:14 +0800

    Solve problem 7

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let letters = string.split('');
+    let reversedWord = "";
+
+    for (let i = letters.length - 1; i >= 0; i--){
+        reversedWord += letters[i]; 
+    }
+
+    return reversedWord;
 
 };
 
